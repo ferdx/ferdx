85568f493dd1211ca583a4a93864e2cdc58b5ee3
Nick Salloum <nick@callmenick.com>
2015-08-28 20:05:22 -0700

    (refactor) refactors the client and server structure and includes angular

---
+++ app/components/hello/hello.js
@@ -0,0 +1,40 @@
+/**
+ * This is the default controller structure that exhibits best practices for
+ * setting up a controller and using it. Always wrap it in an IIFE for safety!
+ */
+(function() {
+
+  /**
+   * Always declare 'use strict' at the top
+   */
+  'use strict';
+
+  /**
+   * Require the base level module and controller
+   */
+  angular
+    .module('app')
+    .controller('HelloController', HelloController);
+
+  /**
+   * HelloController is where all the actual controller functionality resides.
+   */
+  function HelloController() {
+    /**
+     * Use a capture variable for this when using the controllerAs syntax.
+     * Choose a consistent variable name such as vm, which stands for ViewModel.
+     *
+     * Why?: The this keyword is contextual and when used within a function
+     * inside a controller may change its context. Capturing the context of this
+     * avoids encountering this problem.
+     */
+    var vm = this;
+
+    /**
+     * A sample attachment of a controller param, made accessible inside the
+     * HTML wherever a controller is declared.
+     */
+    vm.username = 'Nick';
+  }
+
+})();
