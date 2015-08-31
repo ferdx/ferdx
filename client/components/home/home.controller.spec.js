2d49ebf70fdf809e22a3efc49d8aa59a5689395a
Nick Salloum <nick@callmenick.com>
2015-08-31 11:22:34 -0700

    (refactor) refactors file names for consistency and readability

---
+++ client/components/home/home.controller.spec.js
@@ -0,0 +1,41 @@
+/**
+ * Unit tests for the HomeController
+ * 
+ * @param {String} The controller name
+ */
+describe('HomeController', function() {
+
+  /**
+   * Get app module before each
+   */
+  beforeEach(module('app'));
+
+  /**
+   * Cache some vars for global test usage
+   */
+  var $controller;
+  var vm;
+
+  /**
+   * beforeEach runs some stuff before each test. Returns nothing.
+   * 
+   * @param {Function} injects the controller and sets up the vm variable for
+   *   use in all the tests.
+   */
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+    vm = $controller('HomeController');
+  }));
+
+  /**
+   * An empty test. Returns nothing.
+   * 
+   * @param {String} the test name
+   */
+  describe('empty test', function() {
+    it('should do something', function() {
+
+    });
+  });
+  
+});
