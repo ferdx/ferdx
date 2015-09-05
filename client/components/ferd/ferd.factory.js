df81618746835799cef4238f270cb86318e62086
Timothy Quach <timothyquachbot@gmail.com>
2015-09-04 18:26:05 -0700

    (feat) Validate slack key against slack org when asked to input slack key

--- client/components/ferd/ferd.factory.js
+++ client/components/ferd/ferd.factory.js
@@ -9,10 +9,11 @@
     .module('app')
     .factory('ferdFactory', ferdFactory);
 
-    function ferdFactory($http) {
+    function ferdFactory($q, $http, authFactory) {
 
       factory = {
-        getAvailableModules: getAvailableModules
+        getAvailableModules: getAvailableModules,
+        verifyKey: verifyKey
       };
 
       return factory;
@@ -29,6 +30,27 @@
           .then(function(response) {
             return response;
           });
+      };
+
+      /**
+       * Verifies if slack api key is valid and if it is, that it belongs to
+       * the same org as user's submission.
+       * @param {String} apikey to check
+       * @return {Promise}
+       */
+      function verifyKey(apikey) {
+        var url = 'https://slack.com/api/rtm.start?token=' + apikey;
+        return $http.get(url)
+          .then(function(response) {
+            return $q(function(resolve, reject) {
+              if(response.data && response.data.team &&
+                response.data.team.domain === authFactory.authUser.slackOrganization) {
+                resolve(true);
+              } else {
+                reject(false);
+              }
+            });
+          })
       };
 
     }
