b6d88f11ddb399b2d3b7675238d52e9bfa89a4ce
Nick Salloum <nick@callmenick.com>
2015-09-09 19:18:41 -0700

    (feat) implements marketplace search bar and module filtering

--- client/components/marketplace/marketplace.controller.js
+++ client/components/marketplace/marketplace.controller.js
@@ -17,6 +17,58 @@
    */
   function MarketplaceController() {
     var vm = this;
+
+    // TODO: Actually make this dynamic
+    vm.modules = [
+      {
+        name: 'yo',
+        title: 'Yo!',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem temporibus natus tenetur veniam saepe necessitatibus accusamus.',
+        created_at: 'September 7th, 2015'
+      },
+      {
+        name: 'bart',
+        title: 'Bart',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem temporibus natus tenetur veniam saepe necessitatibus accusamus.',
+        created_at: 'September 7th, 2015'
+      },
+      {
+        name: 'eliza',
+        title: 'Eliza',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem temporibus natus tenetur veniam saepe necessitatibus accusamus.',
+        created_at: 'September 7th, 2015'
+      },
+      {
+        name: 'directions',
+        title: 'Directions',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem temporibus natus tenetur veniam saepe necessitatibus accusamus.',
+        created_at: 'September 7th, 2015'
+      },
+      {
+        name: 'moo',
+        title: 'Moo',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem temporibus natus tenetur veniam saepe necessitatibus accusamus.',
+        created_at: 'September 7th, 2015'
+      },
+      {
+        name: 'schedule',
+        title: 'Schedule',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem temporibus natus tenetur veniam saepe necessitatibus accusamus.',
+        created_at: 'September 7th, 2015'
+      },
+      {
+        name: 'burrito',
+        title: 'Burrito',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem temporibus natus tenetur veniam saepe necessitatibus accusamus.',
+        created_at: 'September 7th, 2015'
+      },
+      {
+        name: 'trinidad',
+        title: 'Trinidad',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem temporibus natus tenetur veniam saepe necessitatibus accusamus.',
+        created_at: 'September 7th, 2015'
+      }
+    ];
   }
 
 })();
