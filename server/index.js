d906590b6120578895a7c5cb6b9829417d92151c
Andrew Kishino <AndrewKishino@gmail.com>
2015-09-10 19:35:21 -0700

    Added mongo variables

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 // load up .env as soon as possible
 require('dotenv').load();
 
+var cred = process.env.MONGOLAB_CRED;
+var dbhost = process.env.MONGOLAB_HOST;
+var dbport = process.env.MONGOLAB_PORT;
+var dbname = process.env.MONGOLAB_DBNAME;
+var dburi = 'mongodb://' + cred + '@' + dbhost + ':' + dbport + '/' + dbname;
+
 // connect and fire up
-var mongoose = require('mongoose').connect('mongodb://localhost/ferdx');
+var mongoose = require('mongoose').connect(dburi);
 var app = require('./config/server.js');
 
 // listen
